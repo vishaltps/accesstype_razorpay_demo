4e35cce049a1f40340b0abb182d7b82c045c3781
= <=>
2021-03-22 12:06:36 +0530

    initial commit

---
+++ lib/accesstype_razorpay_demo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "accesstype_razorpay_demo/version"
+
+module AccesstypeRazorpayDemo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/accesstype_razorpay_demo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AccesstypeRazorpayDemo
+  VERSION = "0.1.0"
+end
